9c66d3a6ddb43623c7d2bec9b359f33892f85094
Furi Lama <lamafuri60@gmail.com>
2026-06-05 10:59:36 +0545

    fix(cors) : remove hardcoded origin to read it from the .env file

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://127.0.0.1:5174'],
+  origin: process.env.CLIENT_URL || 'http://localhost:5174',
   credentials: true,
 }));
 app.use(express.json());
